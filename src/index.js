7ce2a09f393607b3d05a65c84a26c18f3b8c9c08
CrocodileGenna <yuramus1998@gmail.com>
2022-11-10 15:48:03 +0200

    first comit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import './css/styles.css';
+
+const DEBOUNCE_DELAY = 300;
+
+const variables = {
+  input: document.querySelector('#search-box'),
+  ul: document.querySelector('.country-list'),
+  cardCauntry: document.querySelector('.country-info'),
+};
